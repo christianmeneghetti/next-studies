30964d73f55313d43ded342b0171d904fa9ee5fc
Christian <chris333meneghetti@gmail.com>
2024-08-22 21:07:25 -0300

    feat: creating services getMemes and first usage

---
+++ src/services/memes/memes.types.ts
@@ -0,0 +1,19 @@
+export type Meme = {
+  id: number
+  name: string
+  img: string
+  imgAlt: string
+  description: string
+  attackPower: number
+  hittingPoints: number
+}
+
+export type MemesResponseApi = {
+  id: number
+  name: string
+  img: string
+  img_alt: string
+  description: string
+  attack_power: number
+  hitting_points: number
+}
