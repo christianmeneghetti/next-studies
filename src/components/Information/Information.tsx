7246f5193c970b72bfa7400b8359c313434d3d58
Christian <chris333meneghetti@gmail.com>
2024-08-25 14:05:52 -0300

    feat: creating information component

---
+++ src/components/Information/Information.tsx
@@ -0,0 +1,28 @@
+import { DescriptionProps } from './Information.types'
+
+export function Information({ information }: DescriptionProps) {
+  const { name, description, attackPower, hittingPoints } = information
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-zinc-900 rounded-md overflow-hidden w-[500px]">
+      <div className="flex justify-center bg-zinc-700 w-full p-4 font-bold">
+        {name}
+      </div>
+      <div className="flex justify-center p-4 text-justify h-[152px] overflow-y-auto">
+        {description}
+      </div>
+      <div className="flex flex-wrap justify-between items-center w-full p-4 pt-0 gap-1">
+        <div className="flex items-center gap-1">
+          <span className="material-symbols-outlined text-4xl">swords</span>
+          <b className="text-4xl">{attackPower}</b>
+        </div>
+        <div className="flex items-center gap-1">
+          <span className="material-symbols-outlined text-red-500 text-4xl">
+            favorite
+          </span>
+          <b className="text-4xl">{hittingPoints}</b>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Information/Information.types.ts
@@ -0,0 +1,11 @@
+type Information = {
+  id: number
+  name: string
+  description: string
+  attackPower: number
+  hittingPoints: number
+}
+
+export type DescriptionProps = {
+  information: Information
+}
