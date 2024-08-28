948207f0221594f9a50163b6d208b4ce11b95263
Christian <chris333meneghetti@gmail.com>
2024-08-28 20:20:45 -0300

    feat: adjusted ppr service usage and renaming functions and consts

--- src/components/MemePagePPR/MemePagePPR.tsx
+++ src/components/MemePagePPR/MemePagePPR.tsx
@@ -1,23 +1,16 @@
-import { use } from 'react'
 import { Information } from '../Information'
 import { Photo } from '../Photo'
 import { MemesService } from '@/services/memes'
 
-async function fetchDataMemes(memeId: string) {
+export async function MemePagePPR({ memeId }: { memeId: string }) {
   const memesService = new MemesService()
-  const dataMemes = await memesService.getMeme(memeId)
-
-  return dataMemes
-}
-
-export function MemePagePPR({ memeId }: { memeId: string }) {
-  const dataMemes = use(fetchDataMemes(memeId))
-  const { img, imgAlt, ...restDataMemes } = dataMemes
+  const dataMeme = await memesService.getMeme(memeId)
+  const { img, imgAlt, ...restDataMeme } = dataMeme
 
   return (
     <div className="flex fixed items-center justify-center inset-0 bg-zinc-800 -z-10 gap-32">
       <Photo imgSrc={img} imgAlt={imgAlt} />
-      <Information information={restDataMemes} />
+      <Information information={restDataMeme} />
     </div>
   )
 }
