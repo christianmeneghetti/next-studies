1ccd9973834e2b982b2bb3bdffd08b9bd73b59be
Christian <chris333meneghetti@gmail.com>
2024-11-06 21:37:57 -0300

    feat: adding listing to fetchs methods

---
+++ src/app/meme/client-side/page.tsx
@@ -0,0 +1,9 @@
+import { CardsCSR } from '@/components/Cards/CardsCSR'
+
+export default function CSRListing() {
+  return (
+    <div className="flex fixed inset-0 flex-col items-center justify-center p-2">
+      <CardsCSR />
+    </div>
+  )
+}
---
+++ src/app/meme/server-side/page.tsx
@@ -0,0 +1,9 @@
+import { CardsSSR } from '@/components/Cards/CardsSSR'
+
+export default function SSRListing() {
+  return (
+    <div className="flex fixed inset-0 flex-col items-center justify-center p-2">
+      <CardsSSR />
+    </div>
+  )
+}
